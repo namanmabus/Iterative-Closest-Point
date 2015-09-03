0db2c31faf4453c737606c27ab4c18e0f4a2eb87
namanp22 <namanpatel05@gmail.com>
2015-09-03 16:10:32 -0400

    'Iterativeclosestpoint.m'

---
+++ Iterativeclosestpoint.m
@@ -0,0 +1,64 @@
+function [ TR,TT,source ] = Iterativeclosestpoint( model,source,ErrVal )
+%Iterative closest point function with model data and source data as input
+%as well as the maximum accepted error between model and source data.
+%Translational matrix, Rotation Matrix and new source data after rotation
+%and translation as output.
+
+m=size(model,1);  %Dimension of model data
+M=size(model,2);  %Number of points of model data
+N=size(source,2); %Number of points of the source data
+
+TR=eye(m);  %Rotation Matrix initalized as a unit vector
+TT=zeros(m,1); % Translational Matrix initialized with all elements equal to zero
+
+vi=zeros(size(source,2)); % Variable for indexing and mapping the closest points in model to the source
+Err1=100; % Initializing Error variable for error between the model and the source
+while 1   % while loop will run untill the error is sufficiently low as defined by ErrVal
+    for i=1:1:N   % Function for finding the closest point in model corresponding to each point in the source.
+    minval=9e99;
+    for j=1:1:M
+        val=norm(source(:,i)-model(:,j));
+                    if val<minval
+                        minval=val;
+                        vi(i)=j; % Index of the closest point is stored in vi
+                    end
+    end
+    end
+
+Cs=mean(source,2); % Centroid of Source data
+Cm=mean(model,2);  % Centroid of Model Data
+source1=zeros(size(source,1),size(source,2)); % New variable source1 is defined
+p1=zeros(size(source,1),size(source,2)); % New variable p1 is defined
+for i=1:1:N
+    source1(:,i)=source(:,i)-Cs; % Source1 is the value of the source after removing centroid of the source from it.
+    p1(:,i)=model(:,vi(i))-Cm;  % p1 is the value of the closest model points to source after removing centroid of the model from it.
+end
+
+H=source1*p1'; % Defined a matrix H for getting its Singular value decomposition
+[U,D,V]=svd(H); % Singular value decomposition of H
+
+R=V*U';  %
+T=Cm-R*Cs;
+
+TR=R*TR;
+TT=R*TT+T;
+
+source=R*source;
+
+for i=1:1:m
+        source(i,:)=source(i,:)+T(i);
+end
+Err=0;
+for i=1:1:N
+    Err=Err+norm(model(:,vi(i))-source(:,i));
+end
+
+if abs(Err1-Err/N)<ErrVal
+    break;
+else
+    Err1=Err/N;
+end
+end
+
+end
+
---
+++ program.m
@@ -0,0 +1,86 @@
+clc;
+clear all;
+
+fprintf('Loading and Visualizing 2D Line Data ...\n')
+load('2D_Line.mat');
+
+figure();
+plot(model(1,:),model(2,:),'r.',source(1,:),source(2,:),'b.'), axis equal
+title('Original data points (blue) and model points (red)')
+
+fprintf('Program paused. Press enter to apply Iterative Closest Point Algorithm\n');
+pause;
+
+[ TR,TT,source ] = Iterativeclosestpoint( model,source,0.0001 );
+
+figure();
+plot(model(1,:),model(2,:),'r.',source(1,:),source(2,:),'b.'), hold on, axis equal
+title('Original data points (blue) and model points (red)')
+
+clear all;
+fprintf('Program paused. Press enter to Load 2D Line Noise Data\n');
+pause;
+close all;
+fprintf('Loading and Visualizing 2D Line Noise Data ...\n')
+load('2D_Line_Noise.mat');
+
+figure();
+plot(model(1,:),model(2,:),'r.',source(1,:),source(2,:),'b.'), axis equal
+title('Original data points (blue) and model points (red)')
+
+fprintf('Program paused. Press enter to apply Iterative Closest Point Algorithm\n');
+pause;
+
+[ TR,TT,source ] = Iterativeclosestpoint( model,source,0.0001 );
+
+figure();
+plot(model(1,:),model(2,:),'r.',source(1,:),source(2,:),'b.'), hold on, axis equal
+title('Original data points (blue) and model points (red)')
+
+clear all;
+
+fprintf('Program paused. Press enter to Load 3D Cat Data\n');
+pause;
+close all;
+fprintf('Loading and Visualizing 3D Cat Data ...\n')
+load('3D_Cat.mat');
+
+figure();
+plot3(model(1,:),model(2,:),model(3,:),'r.',source(1,:),source(2,:),source(3,:),'b.'), hold on, axis equal
+plot3([1 1 0],[0 1 1],[0 0 0],'r-',[1 1],[1 1],[0 1],'r-','LineWidth',2)
+title('Original data points (blue) and model points (red)')
+
+fprintf('Program paused. Press enter to apply Iterative Closest Point Algorithm\n');
+pause;
+
+[ TR,TT,source ] = Iterativeclosestpoint( model,source,0.0001 );
+
+figure();
+plot3(model(1,:),model(2,:),model(3,:),'r.',source(1,:),source(2,:),source(3,:),'b.'), hold on, axis equal
+plot3([1 1 0],[0 1 1],[0 0 0],'r-',[1 1],[1 1],[0 1],'r-','LineWidth',2)
+title('Original data points (blue) and model points (red)')
+
+
+
+clear all;
+
+fprintf('Program paused. Press enter to Load 3D Cat Noise Data\n');
+pause;
+close all;
+fprintf('Loading and Visualizing 3D Cat Noise Data ...\n')
+load('3D_Cat_Noise.mat');
+
+figure();
+plot3(model(1,:),model(2,:),model(3,:),'r.',source(1,:),source(2,:),source(3,:),'b.'), hold on, axis equal
+plot3([1 1 0],[0 1 1],[0 0 0],'r-',[1 1],[1 1],[0 1],'r-','LineWidth',2)
+title('Original data points (blue) and model points (red)')
+
+fprintf('Program paused. Press enter to apply Iterative Closest Point Algorithm\n');
+pause;
+
+[ TR,TT,source ] = Iterativeclosestpoint( model,source,0.0001 );
+
+figure();
+plot3(model(1,:),model(2,:),model(3,:),'r.',source(1,:),source(2,:),source(3,:),'b.'), hold on, axis equal
+plot3([1 1 0],[0 1 1],[0 0 0],'r-',[1 1],[1 1],[0 1],'r-','LineWidth',2)
+title('Original data points (blue) and model points (red)')
